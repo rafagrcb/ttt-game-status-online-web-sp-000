ad84818ce095fc4b564cd8ed655eac7df8710c66
Rafael Garcia <rafagrcb@gmail.com>
2020-04-05 22:05:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,61 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8], 
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8], 
+  [0, 4, 8],
+  [2, 4, 6],
+  ]
+  
+def won?(board)
+  WIN_COMBINATIONS.each do |single_win_combo|
+    win_index_1 = single_win_combo[0]
+    win_index_2 = single_win_combo[1]
+    win_index_3 = single_win_combo[2]
+    
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+   
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+      return single_win_combo
+    end
+  end
+  return false
+end
+
+def full?(board)
+   board.all? {|i| i == "X" || i == "O"}
+end
+
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif!full?(board) && !won?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
+
+def over?(board)
+  if draw?(board) || won?(board) || full?(board) 
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if x_win = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+    return "X"
+  else o_win = ["X", "O", " ", " ", "O", " ", " ", "O", "X"]
+    return "O"
+  end
+end
